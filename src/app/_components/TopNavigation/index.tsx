ff86c1c20b0e8515c258bdc1df08ffc1598a0139
Austin Whitbeck <AustinWhitbeckArt@gmail.com>
2024-07-05 13:10:52 -0400

    Custom Theme set up (initially for testing)

--- src/app/_components/TopNavigation/index.tsx
+++ src/app/_components/TopNavigation/index.tsx
@@ -1,12 +1,15 @@
 
 const TopNavigation = () => {
   return (
-    <div className="bg-red-800 flex justify-between">
-        <ul>
+    <div className="bg-green-100 flex flex-row justify-between items-center h-12 p-4">
+        <ul className="flex flex-row items-center gap-4 w-full">
+            <li>
+                <img src="https://cdn.bsky.app/img/avatar/plain/did:plc:f6wrpysr4utwq444eql6vbgy/bafkreicights4zxifuf7s255berhn244o6sr3an5wepe5lj4rtobacfy6u@jpeg" className="h-10 rounded-full" />
+            </li>
             <li className="text-blue-400">
                 Home
             </li>
-                        <li className="text-blue-400">
+            <li className="text-blue-400">
                 Home
             </li>
         </ul>
